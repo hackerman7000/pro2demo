9346f86940c17492058e869a40b0b34c1fe79b68
123456 <100360904+hackerman7000@users.noreply.github.com>
2022-06-01 14:40:16 +0200

    div. kleine Anpassungen

--- main.py
+++ main.py
@@ -7,25 +7,29 @@ import json
 import plotly.express as px
 from plotly.offline import plot
 
-
 app = Flask("__name__")
 
 # dict wird erstellt, pro modul wird noch ein unter-dict eingebaut mit den Werten für die Berechung für die ECTS
 #
 faecher = {
     "englischb2": {"ects": 4, "gruppe": "pflicht"},
-           }
+}
+
 
 @app.route('/')
 def hello_home():
     return render_template('index.html')
 
+
 @app.route('/ects.html', methods=['get', 'post'])
 def hello_ects():
-    if request.method =='POST':
+    global L
+
+    if request.method == 'POST':
 
         # Liste L wird erstellt, Module werden falls angewählt in die Liste aufgenommen
         L = []
+
 
         # ECTS-Punke fürs Studium und die einzelnen Module, um damit den Fortschritt berechnen zu können
         ECTS = 180
@@ -54,7 +58,6 @@ def hello_ects():
                 L.append(key)
                 ECTS = ECTS - faecher[key]["ects"]
 
-
         for key, value in faecher.items():
             if request.form.get(key):
                 print("TRUE")
@@ -62,25 +65,12 @@ def hello_ects():
                 L.append(key)
                 ECTS = ECTS - faecher[key]["ects"]
 
+    return render_template('ects.html', liste=L, ECTS=ECTS)
 
-
-    return render_template('ects.html', liste=L, ECTS = ECTS)
 
 @app.route('/noten.html')
 def hello_noten():
     return render_template('noten.html')
-
-
-def get_data():
-    modulgruppen = ["UX", "IT", "DI", "SM"]
-    absolvierte_ects = [4, 0, 0, 0]
-    return modulgruppen, absolvierte_ects
-
-def viz():
-    modulgruppe, absolvierte_ects = get_data()
-    fig = px.bar(x=modulgruppe, y=absolvierte_ects)
-    div = plot(fig, output_type="div")
-    return div
 
 
 @app.route('/auswertung.html')
@@ -88,6 +78,18 @@ def hello_auswertung():
     div = viz()
     return render_template('auswertung.html', viz_div=div)
 
+
+def get_data():
+    modulgruppen = ["UX", "IT", "DI", "SM"]
+    absolvierte_ects = [4, 0, 0, 0]
+    return modulgruppen, absolvierte_ects
+
+
+def viz():
+    modulgruppe, absolvierte_ects = get_data()
+    fig = px.bar(x=modulgruppe, y=absolvierte_ects)
+    div = plot(fig, output_type="div")
+    return div
 
 
 if __name__ == "__main__":
