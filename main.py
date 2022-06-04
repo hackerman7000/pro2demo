f268748bfe806d54d638742ece6b8c9947ed4716
123456 <100360904+hackerman7000@users.noreply.github.com>
2022-06-04 18:35:18 +0200

    Kommentieren

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ from plotly.offline import plot
 app = Flask("__name__")
 
 
+# Startseite
 @app.route('/', methods=["GET", "POST"])
 def hello_home():
+
     # zwei leere Listen werden eröffnet. Diese werden auf der index.html Seite angezeigt
     module_pflicht = []
     module_wp = []
 
-    # zwei leere Listen werden eröffnet. Diese werden mit den absolvierten modulen gefüllt und unten auf der index.html Seite angezeigt
+    # zwei leere Listen werden eröffnet. Diese werden mit den absolvierten modulen gefüllt und unten auf der index.html unten Seite angezeigt
     module_pflicht_absolviert = []
     module_wp_absolviert = []
 
@@ -45,14 +47,14 @@ def hello_home():
         else:
             module_wp.append(element["Modul"])
 
+    # Die Gleiche for Schleife wird nochmals durchlaufen.
+    # Dabei werden jetzt die Absolvierten Module in die Listen "module_pflicht_absolviert" und "module_wp_absolviert" gespeichert
     for element in datei_inhalt:
         if element["Absolviert"] == True:
             if element["Modulgruppe"] == "Pflicht":
                 module_pflicht_absolviert.append(element["Modul"])
             else:
                 module_wp_absolviert.append(element["Modul"])
-
-            # alt: module_wp.append([element["Modul"], element["Modulgruppe"]])
 
     # Wenn der "Zurücksetzen" Button geklickt wird, werden die Module alle auf false (nicht absolviert) gestellt
     # Zudem werden die ECTS aus ects.json auf den Startwert zurückgesetzt
@@ -82,10 +84,10 @@ def hello_home():
     return render_template('index.html', module_pflicht=module_pflicht, module_wp=module_wp, module_pflicht_absolviert=module_pflicht_absolviert, module_wp_absolviert=module_wp_absolviert)
 
 
-
-
+# Seite "ECTS"
 @app.route('/ects.html', methods=['get', 'post'])
 def hello_ects():
+
     # die module werden aus der Datei module.json geladen als datei_inhalt.
     # falls das nicht klappt, wird eine leere Liste eröffnet
     try:
@@ -106,9 +108,11 @@ def hello_ects():
     if request.method == 'POST':
 
         # Wenn ein Modul ausgewähl wurde wird der Wert (falls er noch False ist) von "Absolviert" auf True gesetzt
-        # Zusätzlich wird der ECTS Wert des Moduls dem Gesamtwert der ECTS abgezogen
+        # Zusätzlich wird der ECTS Wert des Moduls dem Wert "ECTS" und dem ECTS_Wert der jeweiligen Modulgruppe abgezogen
         for element in datei_inhalt:
             if request.form.get(element["Modul"]):
+
+                # Durch diese if Abfrage wird verhindert, dass ein Modul mehrmals ausgewählt wird
                 if element["Absolviert"] == False:
                     element["Absolviert"] = True
                     ects_inhalt["ECTS"] = ects_inhalt["ECTS"] - element["ECTS"]
@@ -137,42 +141,63 @@ def hello_ects():
     return render_template('ects.html', ECTS=ects_inhalt["ECTS"], ECTS_UX_Gesamt=ects_inhalt["ECTS_UX_Gesamt"], ECTS_IT_Gesamt=ects_inhalt["ECTS_IT_Gesamt"], ECTS_DI_Gesamt=ects_inhalt["ECTS_DI_Gesamt"], ECTS_SM=ects_inhalt["ECTS_SM"], ECTS_UX_Major=ects_inhalt["ECTS_UX_Major"], ECTS_IT_Major=ects_inhalt["ECTS_IT_Major"], ECTS_DI_Major=ects_inhalt["ECTS_DI_Major"])
 
 
-
+# Seite "Auswertung"
 @app.route('/auswertung.html')
 def hello_auswertung():
+
+    # "div" wird eröffnet. Dieses wird im return dann als viz_div übergeben
     div = viz()
 
+    # die ECTS Punkte werden aus der Datei ects.json geladen als ects_inhalt
+    # falls das nicht klappt, wird ein leeres Dictionary eröffnet
     try:
         with open("data/ects.json") as open_file:
             ects_inhalt = json.load(open_file)
     except FileNotFoundError:
         ects_inhalt = {}
 
+    # return auf auswertung.html und Übergabe des Diagramms
     return render_template('auswertung.html', viz_div=div)
 
 
+# Hier werden die Daten für das Diagramm aus Plotly definiert
 def get_data():
+
+    # die Modulgruppen werden hier definiert und sind fix, deshalb können sie manuell eingegeben werden
     modulgruppen = ["User Experience", "Information Technology", "Digital Innovation"]
+
+    # Eine leere Liste wird eröffnet, in welche die ECTS-Werte aus "ects.json" dynamisch geladen werden
     absolvierte_ects = []
+
+    # die ECTS Punkte werden aus der Datei ects.json geladen als ects_inhalt
+    # falls das nicht klappt, wird ein leeres Dictionary eröffnet
     try:
         with open("data/ects.json") as open_file:
             ects_inhalt = json.load(open_file)
     except FileNotFoundError:
         ects_inhalt = {}
 
+    # Damit nur die gewünschten Werte geladen werden, wird mit einer if Abfrage sichergestellt, dass nur die Werte welche "_Gesamt" enthalten geladen werden
+    # Diese drei Werte werden umgerechnet, damit man sie in der Grafik richtig anzeigen kann und in die Liste "absolvierte_ects" geladen
     for key, value in ects_inhalt.items():
         if "_Gesamt" in key:
             absolvierte_ects.append(8 - value)
     return modulgruppen, absolvierte_ects
 
-
+# Das Plotly Diagramm wird hier erstellt
 def viz():
     modulgruppe, absolvierte_ects = get_data()
+
+    # Die Modulgruppen bilden die x-Achse und die ECTS Punkte die y-Achse
     fig = px.bar(x=modulgruppe, y=absolvierte_ects)
+
+    # Gestaltung des Diagramms. Roter (Minimum ECTS) und Grüner Bereich (Major) wird ins Diagramm hinzugefügt
+    # Quelle für Diagrammgestaltung: https://plotly.com/python/styling-plotly-express/
     fig.add_hrect(y0=0, y1=8, line_width=0, fillcolor="red", opacity=0.2, annotation_text="Minimum nicht erfüllt", annotation_position="top right")
     fig.add_hrect(y0=20, y1=40, line_width=0, fillcolor="green", opacity=0.2, annotation_text="Major erreicht", annotation_position="top right")
     div = plot(fig, output_type="div")
 
+    # Übergabe des Diagrams
     return div
 
 
